fe448b731e3d90180a62f4c56d4477aa0085c4f5
edwin martinez <edwmartinezg@outlook.com>
2023-08-28 19:59:36 -0500

    Arquitectura del proyecto

--- BlazorExpenseTracker.UI/Program.cs
+++ BlazorExpenseTracker.UI/Program.cs
@@ -1,4 +1,3 @@
-using BlazorExpenseTracker.UI.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -13,7 +12,7 @@ namespace BlazorExpenseTracker.UI
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
-            builder.Services.AddSingleton<WeatherForecastService>();
+            
 
             var app = builder.Build();
 
